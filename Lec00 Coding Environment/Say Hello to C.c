26a7b222f4af177c4c8ac154d1d211bc73d58ad1
dadavidtseng <dadavidtseng@gmail.com>
2023-10-02 13:59:44 +0800

    test commit

---
+++ Lec00 Coding Environment/Say Hello to C.c
@@ -0,0 +1,28 @@
+/*
+Welcome to program design (I). Let's say hello to our best friend C.
+
+Input
+No input
+
+Output
+Output a single line "Hello C!"
+
+Example 1
+
+Input
+(No input)
+
+Output
+Hello C!
+
+Limits
+Your program needs to finish task in 1 second.
+Your program can only use memory less than 10000 KB.
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello C!\n");
+    return 0;
+}
